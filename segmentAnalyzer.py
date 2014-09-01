1c525453b988da9f23a7dee42a079d0300e79b1d
Robert <mam09cqz@studserv.uni-leipzig.de>
2014-09-01 16:36:23 +0200

    created function 'createSubentries'

--- segmentAnalyzer.py
+++ segmentAnalyzer.py
@@ -10,11 +10,11 @@ from vicar import Vicar
 from analyzer import Analyzer
 import re
 import xml.etree.ElementTree as ElementTree
-import datetime
-import dateutil.parser
 
 
 class SegmentAnalyzer:
+
+    eventId = 0
 
     def __init__(self, segment, vicar):
         self.segment = segment
@@ -80,9 +80,6 @@ class SegmentAnalyzer:
             date[1] = 'után'
         elif ('?' in text):
             date[1] = '?'
-        #if (re.search('\bk.\b|\be.\b|\belőtt\b|\bu.\b|\bután\b|[?]', text)):
-            #date[1] = re.findall('\bk.\b|\be.\b|\belőtt\b|\bu.\b|\bután\b|[?]', text)[0]
-        #print (date)
         return date
 
     def analyzeBrackets(self, text):
@@ -115,8 +112,6 @@ class SegmentAnalyzer:
                     analyzed[0] = 'után'
                 elif ('?' in bracket):
                     analyzed[0] = '?'
-                #if (re.search('\bk.\b|\be.\b|\belőtt\b|\bu.\b|\bután\b|[?]', bracket)):
-                    #analyzed[0] = re.findall('\bk.\b|\be.\b|\belőtt\b|\bu.\b|\bután\b|[?]', bracket)[0]
             #bracket contains place
             else:
                 analyzed[2] = bracket
@@ -145,8 +140,6 @@ class SegmentAnalyzer:
                     analyzed[0] = 'után'
                 elif ('?' in bracket1):
                     analyzed[0] = '?'
-                #if (re.search('\bk.\b|\be.\b|\belőtt\b|\bu.\b|\bután\b|[?]', bracket1)):
-                    #analyzed[0] = re.findall('\bk.\b|\be.\b|\belőtt\b|\bu.\b|\bután\b|[?]', bracket1)[0]
             else:
                 analyzed[2] = bracket1
             #bracket2 handling
@@ -169,12 +162,47 @@ class SegmentAnalyzer:
                     analyzed[0] = 'után'
                 elif ('?' in bracket2):
                     analyzed[0] = '?'
-                #if (re.search('\bk.\b|\be.\b|\belőtt\b|\bu.\b|\bután\b|[?]', bracket2)):
-                    #analyzed[0] = re.findall('\bk.\b|\be.\b|\belőtt\b|\bu.\b|\bután\b|[?]', bracket2)[0]
             else:
                 analyzed[2] = bracket2
-        #print (analyzed)
         return analyzed
+
+    def createSubentries(self, vicarElement, elementName, entry, idToggle):
+        if (idToggle):
+            for element in vicarElement:
+                elementEntry = ElementTree.SubElement(entry, elementName)
+                elementEntry.set('id', str(self.vicar.id) + '-' + str(self.eventId))
+                self.eventId += 1
+                if ('(' in element):
+                    ElementTree.SubElement(elementEntry, 'name').text = element[:element.find('(')].strip()
+                    elementData = self.analyzeBrackets(element)
+                    if (elementData[0] is not '0'):
+                        if (len(elementData[1]) > 3):
+                            ElementTree.SubElement(elementEntry, 'date').text = elementData[1].strip()
+                            elementEntry.set('inaccuracy', str(elementData[0].strip()))
+                    else:
+                        if (len(elementData[1]) > 3):
+                            ElementTree.SubElement(elementEntry, 'date').text = elementData[1].strip()
+                    if (len(elementData[2]) > 2):
+                        ElementTree.SubElement(elementEntry, 'place').text = elementData[2].strip()
+                else:
+                    ElementTree.SubElement(elementEntry, 'name').text = element.strip()
+        else:
+            for element in vicarElement:
+                elementEntry = ElementTree.SubElement(entry, elementName)
+                if ('(' in element):
+                    ElementTree.SubElement(elementEntry, 'name').text = element[:element.find('(')].strip()
+                    elementData = self.analyzeBrackets(element)
+                    if (elementData[0] is not '0'):
+                        if (len(elementData[1]) > 3):
+                            ElementTree.SubElement(elementEntry, 'date').text = elementData[1].strip()
+                            elementEntry.set('inaccuracy', str(elementData[0].strip()))
+                    else:
+                        if (len(elementData[1]) > 3):
+                            ElementTree.SubElement(elementEntry, 'date').text = elementData[1].strip()
+                    if (len(elementData[2]) > 2):
+                        ElementTree.SubElement(elementEntry, 'place').text = elementData[2].strip()
+                else:
+                    ElementTree.SubElement(elementEntry, 'name').text = element.strip()
 
     # Create the XML tree
     def createEntry(self, root):
@@ -245,151 +273,37 @@ class SegmentAnalyzer:
 
         #Siblings
         if len(self.vicar.siblings) > 0:
-            for sibling in self.vicar.siblings:
-                siblingEntry = ElementTree.SubElement(entry, 'sibling')
-                if ('(' in sibling):
-                    ElementTree.SubElement(siblingEntry, 'name').text = sibling[:sibling.find('(')].strip()
-                    siblingData = self.analyzeBrackets(sibling)
-                    if (siblingData[0] is not '0'):
-                        if (len(siblingData[1]) > 3):
-                            ElementTree.SubElement(siblingEntry, 'date').text = siblingData[1].strip()
-                            siblingEntry.set('inaccuracy', str(siblingData[0].strip()))
-                    else:
-                        if (len(siblingData[1]) > 3):
-                            ElementTree.SubElement(siblingEntry, 'date').text = siblingData[1].strip()
-                    if (len(siblingData[2]) > 2):
-                        ElementTree.SubElement(siblingEntry, 'place').text = siblingData[2].strip()
-                else:
-                    ElementTree.SubElement(siblingEntry, 'name').text = sibling.strip()
+            self.createSubentries(self.vicar.siblings, 'sibling', entry, False)
         del(self.vicar.siblings[:])
 
         #Offspring
         if len(self.vicar.offspring) > 0:
-            for offspring in self.vicar.offspring:
-                offspringEntry = ElementTree.SubElement(entry, 'offspring')
-                if ('(' in offspring):
-                    ElementTree.SubElement(offspringEntry, 'name').text = offspring[:offspring.find('(')].strip()
-                    offspringData = self.analyzeBrackets(offspring)
-                    if (offspringData[0] is not '0'):
-                        if (len(offspringData[1]) > 3):
-                            ElementTree.SubElement(offspringEntry, 'date').text = offspringData[1].strip()
-                            offspringEntry.set('inaccuracy', str(offspringData[0].strip()))
-                    else:
-                        if (len(offspringData[1]) > 3):
-                            ElementTree.SubElement(offspringEntry, 'date').text = offspringData[1].strip()
-                    if (len(offspringData[2]) > 2):
-                        ElementTree.SubElement(offspringEntry, 'place').text = offspringData[2].strip()
-                else:
-                    ElementTree.SubElement(offspringEntry, 'name').text = offspring.strip()
+            self.createSubentries(self.vicar.offspring, 'offspring', entry, False)
         del(self.vicar.offspring[:])
 
         #Vicars
         if len(self.vicar.vicars) > 0:
-            for vicar in self.vicar.vicars:
-                vicarEntry = ElementTree.SubElement(entry, 'vicar')
-                if ('(' in vicar):
-                    ElementTree.SubElement(vicarEntry, 'name').text = vicar[:vicar.find('(')].strip()
-                    vicarData = self.analyzeBrackets(vicar)
-                    if (vicarData[0] is not '0'):
-                        if (len(vicarData[1]) > 3):
-                            ElementTree.SubElement(vicarEntry, 'date').text = vicarData[1].strip()
-                            vicarEntry.set('inaccuracy', str(vicarData[0].strip()))
-                    else:
-                        if (len(vicarData[1]) > 3):
-                            ElementTree.SubElement(vicarEntry, 'date').text = vicarData[1].strip()
-                    if (len(vicarData[2]) > 2):
-                        ElementTree.SubElement(vicarEntry, 'place').text = vicarData[2].strip()
-                else:
-                    ElementTree.SubElement(vicarEntry, 'name').text = vicar.strip()
+            self.createSubentries(self.vicar.vicars, 'vicar', entry, False)
         del(self.vicar.vicars[:])
 
-        eventId = 0
         #Pastors
         if len(self.vicar.pastors) > 0:
-            for pastor in self.vicar.pastors:
-                pastorEntry = ElementTree.SubElement(entry, 'pastor')
-                pastorEntry.set('id', str(self.vicar.id) + '-' + str(eventId))
-                eventId += 1
-                if ('(' in pastor):
-                    ElementTree.SubElement(pastorEntry, 'name').text = pastor[:pastor.find('(')].strip()
-                    pastorData = self.analyzeBrackets(pastor)
-                    if (pastorData[0] is not '0'):
-                        if (len(pastorData[1]) > 3):
-                            ElementTree.SubElement(pastorEntry, 'date').text = pastorData[1].strip()
-                            pastorEntry.set('inaccuracy', str(pastorData[0].strip()))
-                    else:
-                        if (len(pastorData[1]) > 3):
-                            ElementTree.SubElement(pastorEntry, 'date').text = pastorData[1].strip()
-                    if (len(pastorData[2]) > 2):
-                        ElementTree.SubElement(pastorEntry, 'place').text = pastorData[2].strip()
-                else:
-                    ElementTree.SubElement(pastorEntry, 'name').text = pastor.strip()
+            self.createSubentries(self.vicar.pastors, 'pastor', entry, True)
         del(self.vicar.pastors[:])
 
         #Institutions
         if len(self.vicar.institutions) > 0:
-            for institution in self.vicar.institutions:
-                institutionEntry = ElementTree.SubElement(entry, 'institution')
-                institutionEntry.set('id', str(self.vicar.id) + '-' + str(eventId))
-                eventId += 1
-                if ('(' in institution):
-                    ElementTree.SubElement(institutionEntry, 'name').text = institution[:institution.find('(')].strip()
-                    institutionData = self.analyzeBrackets(institution)
-                    if (institutionData[0] is not '0'):
-                        if (len(institutionData[1]) > 3):
-                            ElementTree.SubElement(institutionEntry, 'date').text = institutionData[1].strip()
-                            institutionEntry.set('inaccuracy', str(institutionData[0].strip()))
-                    else:
-                        if (len(institutionData[1]) > 3):
-                            ElementTree.SubElement(institutionEntry, 'date').text = institutionData[1].strip()
-                    if (len(institutionData[2]) > 2):
-                        ElementTree.SubElement(institutionEntry, 'place').text = institutionData[2].strip()
-                else:
-                    ElementTree.SubElement(institutionEntry, 'name').text = institution.strip()
+            self.createSubentries(self.vicar.institutions, 'institution', entry, True)
         del(self.vicar.institutions[:])
 
         #Teachers
         if len(self.vicar.teachers) > 0:
-            for teacher in self.vicar.teachers:
-                teacherEntry = ElementTree.SubElement(entry, 'teacher')
-                teacherEntry.set('id', str(self.vicar.id) + '-' + str(eventId))
-                eventId += 1
-                if ('(' in teacher):
-                    ElementTree.SubElement(teacherEntry, 'name').text = teacher[:teacher.find('(')].strip()
-                    teacherData = self.analyzeBrackets(teacher)
-                    if (teacherData[0] is not '0'):
-                        if (len(teacherData[1]) > 3):
-                            ElementTree.SubElement(teacherEntry, 'date').text = teacherData[1].strip()
-                            teacherEntry.set('inaccuracy', str(teacherData[0].strip()))
-                    else:
-                        if (len(teacherData[1]) > 3):
-                            ElementTree.SubElement(teacherEntry, 'date').text = teacherData[1].strip()
-                    if (len(teacherData[2]) > 2):
-                        ElementTree.SubElement(teacherEntry, 'place').text = teacherData[2].strip()
-                else:
-                    ElementTree.SubElement(teacherEntry, 'name').text = teacher.strip()
+            self.createSubentries(self.vicar.teachers, 'teacher', entry, True)
         del(self.vicar.teachers[:])
 
         #Education
         if len(self.vicar.education) > 0:
-            for education in self.vicar.education:
-                educationEntry = ElementTree.SubElement(entry, 'education')
-                educationEntry.set('id', str(self.vicar.id) + '-' + str(eventId))
-                eventId += 1
-                if ('(' in education):
-                    ElementTree.SubElement(educationEntry, 'name').text = education[:education.find('(')].strip()
-                    educationData = self.analyzeBrackets(education)
-                    if (educationData[0] is not '0'):
-                        if (len(educationData[1]) > 3):
-                            ElementTree.SubElement(educationEntry, 'date').text = educationData[1].strip()
-                            educationEntry.set('inaccuracy', str(educationData[0].strip()))
-                    else:
-                        if (len(educationData[1]) > 3):
-                            ElementTree.SubElement(educationEntry, 'date').text = educationData[1].strip()
-                    if (len(educationData[2]) > 2):
-                        ElementTree.SubElement(educationEntry, 'place').text = educationData[2].strip()
-                else:
-                    ElementTree.SubElement(educationEntry, 'name').text = education.strip()
+            self.createSubentries(self.vicar.education, 'education', entry, True)
         del(self.vicar.education[:])
 
         #Misc
